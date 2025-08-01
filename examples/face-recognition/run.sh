ca3a41562e723a91fed3270eaba6e0cc6775cbd5
tgasla <theodoros.aslanidis@ucdconnect.ie>
2025-08-01 15:35:11 +0100

    added face-recognition example. The example is successfully working using the run.sh script

---
+++ examples/face-recognition/run.sh
@@ -0,0 +1,5 @@
+export LD_LIBRARY_PATH=${GVIRTUS_HOME}/lib:${GVIRTUS_HOME}/lib/frontend
+nvcc -shared -Xcompiler -fPIC -o libextension.so extension.cu  -lcudart -lcublas
+ldd libextension.so
+LD_PRELOAD=${GVIRTUS_HOME}/lib/frontend/libcudart.so:${GVIRTUS_HOME}/lib/frontend/libcublas.so \
+python cnn.py
