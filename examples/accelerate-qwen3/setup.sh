6d3074a761d0d960c13502364c8b440d04c98aa4
tgasla <theodoros.aslanidis@ucdconnect.ie>
2025-08-06 19:15:21 +0100

    update .gitignore and example setup scripts

--- examples/accelerate-qwen3/setup.sh
+++ examples/accelerate-qwen3/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 apt update && apt install -y python3 python3-pip
-pip install transformers==4.48.2 accelerate==1.9.0
+pip3 install transformers==4.48.2 accelerate==1.9.0
--- examples/fastapi-qwen3/setup.sh
+++ examples/fastapi-qwen3/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-apt update && apt install -y python3 python3-pip requests
-pip install torch==2.6.0 transformers==4.54.1 accelerate==1.9.0
-pip install flashinfer-python -i https://flashinfer.ai/whl/cu126/torch2.6/
+apt update && apt install -y python3 python3-pip
+pip3 install torch==2.6.0 transformers==4.54.1 accelerate==1.9.0 requests==2.32.4 fastapi==0.116.1 uvicorn[standard]==0.35.0
+pip3 install flashinfer-python -i https://flashinfer.ai/whl/cu126/torch2.6/
