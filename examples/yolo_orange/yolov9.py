751adb6fc0bd04d4abe199e53878f8f9bf5150fa
Ting-Hui Cheng <tinghui1738cheng@gmail.com>
2025-12-04 14:37:34 +0100

    YoloV9 orange detection

---
+++ examples/yolo_orange/yolov9.py
@@ -0,0 +1,28 @@
+# Reference: https://medium.com/@Mert.A/how-to-use-yolov9-for-object-detection-93598ad88d7d
+import cv2
+from ultralytics import YOLO
+
+def predict(chosen_model, img, classes=[], conf=0.5):
+    if classes:
+        results = chosen_model.predict(img, classes=classes, conf=conf)
+    else:
+        results = chosen_model.predict(img, conf=conf)
+
+    return results
+
+def predict_and_detect(chosen_model, img, classes=[], conf=0.5, rectangle_thickness=2, text_thickness=1):
+    results = predict(chosen_model, img, classes, conf=conf)
+    for result in results:
+        for box in result.boxes:
+            cv2.rectangle(img, (int(box.xyxy[0][0]), int(box.xyxy[0][1])),
+                          (int(box.xyxy[0][2]), int(box.xyxy[0][3])), (255, 0, 0), rectangle_thickness)
+            cv2.putText(img, f"{result.names[int(box.cls[0])]}",
+                        (int(box.xyxy[0][0]), int(box.xyxy[0][1]) - 10),
+                        cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), text_thickness)
+    return img, results
+
+model = YOLO("weights/yolov9c.pt")
+
+image = cv2.imread("images/FTP.6.62.6_Fruit-Yield-Back-Picture_1_2021-12-01-12-58-24.jpg")
+result_img, _ = predict_and_detect(model, image, classes=[], conf=0.17)
+cv2.imwrite("output.jpg", result_img)
